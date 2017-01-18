bc9161748d1e92e098cf2b694a68061427ce011f
Dmitriy Yarygin <dmitriy.yarygin@yahoo.com>
2017-01-18 16:31:29 +0200

    Testing bootstrap with CodeSchool

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Blasting Off With Bootstrap</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
+</head>
+<body>
+    <div class='navbar navbar-default'>  
+        <div class='container'>  
+            <div class="navbar-header">
+                <a href="/" class='navbar-brand'>Blasting Off With Bootstrap</a>
+                <button type='button' class="navbar-toggle" data-toggle='collapse' data-target='.navbar-collapse'>
+                    <span class='sr-only'> Toggle navigation</span> 
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                    <span class='icon-bar'></span>
+                </button>
+            </div>
+            <ul class='nav navbar-nav navbar-right collapse navbar-collapse'>
+                <li><a href='tickets.html'>Tickets</a></li>
+                <li><a href='stations.html'>Stations</a></li>
+                <li>
+                    <a href='about.html' data-target='#' data-toggle='dropdown'>
+                        About <span class='caret'></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href='story.html'>Our Story</a></li>
+                        <li><a href='contact.html'>Contact Us</a></li>
+                        <li class='divider'></li>
+                        <li><a href='/blog'>Blog</a></li>
+                        <li class='divider'></li>
+                        <li><a href='http://twitter.com/...'>Twitter</a></li>
+                        <li><a href='http://facebook.com/...'>Facebook</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div> 
+    </div> 
+    <div class='container'> 
+        <div class='row well well-lg'>
+            <div class='col-md-6'>    
+                <h2>The Fastest Way to Space</h2>
+                <p class='lead'>Make your way to space in the comfort of your own rocket, elevator or transporter.</p>
+                <button class='btn btn-lg btn-default' type='button'>Take the Tour</button>
+                <button class='btn btn-lg btn-primary' type='button'>Book Tickets Now</button>
+            </div>
+            <div class='col-md-6 visible-md visible-lg'>    
+                <img style="height: 220px" src='images/Koko 1.jpg' alt='Koko train' />
+            </div>
+        </div>
+    </div>
+    <div class='container-fluid'>
+        <div class='row text-center features'>
+            <div class='col-sm-4  col-xs-10  col-xs-offset-1  col-sm-offset-0'>   
+                <i class='glyphicon glyphicon-briefcase'></i> 
+                <h3>Book Today!</h3>
+                <p>Even if you're traveling tomorrow, you can still get tickets today. We have a
+                number of conveniently located ports around the globe to service everyone.</p>
+            </div>
+            <div class='col-md-3 col-sm-4 col-xs-6'>   
+                <i class='glyphicon glyphicon-random'></i>     
+                <h3>Go Anywhere</h3>
+                <p>If you need to get to space today, why not try out a transporter? Despite the claims,
+                there are have been no deaths in the last 6 weeks!</p>
+            </div>
+            <div class='col-md-3 col-sm-4 col-xs-6'> 
+                <i class='glyphicon glyphicon-send'></i>
+                <h3>RocketBus&reg;</h3>
+                <p>For cheapest fares, catch the next RocketBus® to the stars. Cheaper on your wallet,
+                and easiest way to make friends.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
